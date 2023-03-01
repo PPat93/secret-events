5cf27e338e74936ac17c5ba48f000215abb9355a
Piotr <ptrptrk@gmail.com>
2023-03-01 00:17:45 +0000

    v0.00.02 - create event class, start to slowly add new layout elements

---
+++ app/src/main/java/com/example/eventsapp/CustomEvent.java
@@ -0,0 +1,11 @@
+package com.example.eventsapp;
+
+import java.util.Date;
+
+public class CustomEvent {
+
+    private String title = "";
+    private String description = "";
+    private String image = "";
+//private Date date = TODO
+}
