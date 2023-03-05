ebf1a429c87ef51b46714ff4792e9933fbd00133
Piotr <ptrptrk@gmail.com>
2023-03-05 01:55:38 +0000

    v0.00.02 - update add item layout, some epxeriments with value receiving and displayment + addition of some strings

--- app/src/main/java/com/example/eventsapp/AddView.java
+++ app/src/main/java/com/example/eventsapp/AddView.java
@@ -3,29 +3,49 @@ package com.example.eventsapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddView extends AppCompatActivity {
 
+    Button cancelButton;
     Button okButton;
+    EditText passphraseInput;
+    TextView receivedPass;
 
-    private String title = "";
-    private String description = "";
+    private String title = "Added item: ";
+    private String description = "Next stop: ";
     private String image = "";
-    //private Date date = TODO
-    
+    //private String hour = TODO
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_add);
 
+        cancelButton = findViewById(R.id.cancelButton);
         okButton = findViewById(R.id.okButton);
-        okButton.setOnClickListener(new View.OnClickListener() {
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                receiveNewPass();
+            }
+        });
+    }
+
+    protected String receiveNewPass() {
+        passphraseInput = (EditText) findViewById(R.id.passphraseInput);
+        receivedPass = (TextView) findViewById(R.id.receivedTitle);
+        title += passphraseInput.getText().toString();
+        receivedPass.setText(title);
+        return title;
     }
 }
