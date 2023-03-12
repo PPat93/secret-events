9e07de5a3c389de1d6cff445dc401cf3b497cc7d
Piotr <ptrptrk@gmail.com>
2023-03-12 23:29:26 +0000

    v0.00.02 - some cleaning on code and addition of comments. A little experiments with db

--- app/src/main/java/com/example/eventsapp/MainActivity.java
+++ app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -25,20 +27,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton addPassFab = (FloatingActionButton) findViewById(R.id.newPass);
-        addPassFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openAddView();
-            }
-        });
-        RecyclerView mainEventsList = findViewById(R.id.mainEventsList);
 
+//      Open add pass view
+        FloatingActionButton addPassFab = findViewById(R.id.newPass);
+        addPassFab.setOnClickListener(view -> openAddView());
+
+//      Event list displayment
+        RecyclerView mainEventsList = findViewById(R.id.mainEventsList);
         mainEventsList.setLayoutManager(new LinearLayoutManager(this));
         mainEventsList.setAdapter(new ViewAdapter(getApplicationContext(), events));
         Intent intent = getIntent();
 //        if (intent.getStringExtra("newPass") != null)
 //            events.add(new Event(intent.getStringExtra("newPass"), "Plac bohaterów getta 3, Kraków ", R.drawable.testimg3));
+
+        try {
+            SQLiteDatabase mydatabase = this.openOrCreateDatabase("your database name", Context.MODE_PRIVATE, null);
+        } catch (Exception e) {
+            Log.i("sql error", e.toString());
+        }
     }
 
 
