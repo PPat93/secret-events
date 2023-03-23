cc1b3bda70808c4ee5b577c5f2503e38cab7b24d
Piotr <ptrptrk@gmail.com>
2023-03-23 01:10:49 +0000

    v0.01.02 - proper creation of event details activity, with it's layout, started creation and assignment of data retrieved from Main Activity db event created HashMap, on basis of yhe passed through intent passphrase. Initial layout creation and organization + formatting

--- app/src/main/java/com/example/eventsapp/EventDetails.java
+++ app/src/main/java/com/example/eventsapp/EventDetails.java
@@ -2,20 +2,56 @@ package com.example.eventsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventDetails extends AppCompatActivity {
+    List<String> singleEvent = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
-        Intent passphraseIntent = new Intent();
-        String str = passphraseIntent.getStringExtra("detailedPassphrase");
-        TextView textView2 = findViewById(R.id.textView2);
-        textView2.setText(str + "siema");
+
+//        retrieve passphrase of a clicked event
+        Bundle bundle = getIntent().getExtras();
+        String passphrase = bundle.getString("passphrase");
+
+        singleEvent = MainActivity.dbRecordsRetrieved.get(passphrase);
+        defineAndAssignAllEventData();
+    }
+
+    private void defineAndAssignAllEventData() {
+
+        //  title
+        TextView detailedEventTitle = findViewById(R.id.detailedEventTitle);
+        detailedEventTitle.setText(singleEvent.get(0));
+
+        //   type
+        TextView detailedEventType = findViewById(R.id.detailedEventType);
+        detailedEventType.setText(singleEvent.get(1));
+
+        //   address
+        TextView detailedEventAddress = findViewById(R.id.detailedEventAddress);
+        detailedEventAddress.setText(singleEvent.get(2));
+
+        //   description
+        TextView detailedEventDescription = findViewById(R.id.detailedEventDescription);
+        detailedEventDescription.setText(singleEvent.get(3));
+
+        //   time
+        TextView detailedEventTime = findViewById(R.id.detailedEventTime);
+        detailedEventTime.setText(singleEvent.get(5));
+
+        //   image_name
+        ImageView detailedEventPicture = findViewById(R.id.detailedEventPicture);
+        detailedEventPicture.setImageResource(Shared.getEventImagesMap(singleEvent.get(6)));
+
 
     }
 }
