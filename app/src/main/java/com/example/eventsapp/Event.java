a00ca5235fdbf2663ad994efebd5fdf0f2738fc6
Piotr <ptrptrk@gmail.com>
2023-03-23 01:07:19 +0000

    v0.01.01 - add passphrase property to the event class and an invisible text holder to single recycler item layout so it can be passed to each recycler item, and after a click, passed to the new, detailed activity. prepare detailed event activity calling method, after recycler item click, update all needed items with new passphrase property - getter, event definition, creation, etc. + fromatting and new icon for event description

--- app/src/main/java/com/example/eventsapp/Event.java
+++ app/src/main/java/com/example/eventsapp/Event.java
@@ -2,37 +2,20 @@ package com.example.eventsapp;
 
 import static java.lang.Integer.parseInt;
 
+import android.util.Log;
+
 public class Event {
 
-    String eventTitle, eventAddress, eventTime, eventType, eventPicture, order_number, event_description;
+    String eventTitle, eventAddress, eventTime, eventType, eventPicture, order_number, event_description, event_passphrase;
 
-    public Event(String eventTitle, String eventAddress, String eventTime, String eventType, String eventPicture, String order_number) {
+    public Event(String eventTitle, String eventAddress, String eventTime, String eventType, String eventPicture, String order_number, String event_passphrase) {
         this.eventTitle = eventTitle;
         this.eventAddress = eventAddress;
         this.eventTime = eventTime;
         this.eventType = eventType;
         this.eventPicture = eventPicture;
         this.order_number = order_number;
-    }
-
-    public void setEventTitle(String eventTitle) {
-        this.eventTitle = eventTitle;
-    }
-
-    public void setEventAddress(String eventAddress) {
-        this.eventAddress = eventAddress;
-    }
-
-    public void setevent_description(String event_description) {
-        this.event_description = event_description;
-    }
-
-    public void setEventTime(String eventTime) {
-        this.eventTime = eventTime;
-    }
-
-    public String getevent_description() {
-        return event_description;
+        this.event_passphrase = event_passphrase;
     }
 
     public String getEventTitle() {
@@ -42,7 +25,6 @@ public class Event {
     public String getEventAddress() {
         return eventAddress;
     }
-
 
     public String getEventTime() {
         return eventTime;
@@ -58,5 +40,9 @@ public class Event {
 
     public int getOrderNumber() {
         return parseInt(order_number);
+    }
+
+    public String getEventPassphrase() {
+        return event_passphrase;
     }
 }
