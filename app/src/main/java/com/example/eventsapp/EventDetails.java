b29c627152b34bc60493d37d2aa9028324efd7f6
Piotr <ptrptrk@gmail.com>
2023-03-24 23:54:09 +0000

    v0.01.03 - update event details with automatic google maps opening and address searching after click

--- app/src/main/java/com/example/eventsapp/EventDetails.java
+++ app/src/main/java/com/example/eventsapp/EventDetails.java
@@ -2,7 +2,10 @@ package com.example.eventsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +35,22 @@ public class EventDetails extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        findViewById(R.id.detailedEventAddress).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+//                        on address click open Google Maps with event address searched
+                        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + singleEvent.get(2));
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                }, 1000);
             }
         });
     }
